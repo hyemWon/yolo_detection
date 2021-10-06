9897fd8b9dfe0e3b33a834ad3b38bb5c1a075d2b
Hyewon Jeong <55379636+hyemWon@users.noreply.github.com>
2021-10-06 16:06:33 +0900

    Update socket_client.py
    update socket

--- socket_client.py
+++ socket_client.py
@@ -30,50 +30,51 @@ class ClientSocket(threading.Thread):
         # 영상 송신
         self.send()
 
+
     def send(self):
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]  # 이미지의 품질 설정
 
         start_time = time.time()
         count = 0
+
         while self.cap.isOpened():
-            try:
-                ret, frame = self.cap.read()
-                if ret:
-                    frame = cv2.resize(frame, dsize=(640, 480), interpolation=cv2.INTER_AREA)
 
-                    # 프레임을 string 형태로 인코딩
-                    res, encode_frame = cv2.imencode('.jpg', frame, encode_param)
+            ret, frame = self.cap.read()
+            if ret:
+                frame = cv2.resize(frame, dsize=(640, 480), interpolation=cv2.INTER_AREA)
 
-                    string_data = np.array(encode_frame).tostring()
+                # 프레임을 string 형태로 인코딩
+                res, encode_frame = cv2.imencode('.jpg', frame, encode_param)
 
-                    # string 데이터 socket을 통해 전송
-                    # self.sock.sendall(str(len(string_data)).encode().ljust(16))
-                    # self.sock.sendall(string_data)
-                    self.sock.sendall(str(len(string_data)).encode().ljust(16) + string_data)
+                string_data = np.array(encode_frame).tostring()
 
-                    cv2.putText(frame, str(self.fps), (15, 25), cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 0, 0), 2)
-                    cv2.imshow(str(self.url), frame)
+                # string 데이터 socket을 통해 전송
+                # self.sock.sendall(str(len(string_data)).encode().ljust(16))
+                # self.sock.sendall(string_data)
+                self.sock.sendall(str(len(string_data)).encode().ljust(16) + string_data)
 
-                    count += 1
+                cv2.putText(frame, str(self.fps), (15, 25), cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 0, 0), 2)
+                cv2.imshow(str(self.url), frame)
 
-                    if cv2.waitKey(1) & 0xff == ord('q'):
-                        break
+                count += 1
 
-                    if time.time() - start_time >= 1:
-                        self.fps = int(count)
-                        count = 0
-                        start_time = time.time()
-
-                    time.sleep(0.001)
-            except:
+            if cv2.waitKey(1) & 0xff == ord('q'):
                 break
+
+            if time.time() - start_time >= 1:
+                self.fps = int(count)
+                count = 0
+                start_time = time.time()
+
+            time.sleep(0.001)
         cv2.destroyAllWindows()
 
 
 
 
+
+
 def main():
-    # URL = 'rtsp://admin:4ind331%23@192.168.0.242/profile2/media.smp'
 
     if len(sys.argv)==2:
         URL = sys.argv[1]
@@ -92,15 +93,8 @@ def main():
     client = ClientSocket(SERVER, PORT, URL, HEADER, FORMAT)
     client.start()
 
-    # cap = cv2.VideoCapture(URL)
-    # while cap.isOpened():
-    #     ret, frame = cap.read()
-    #     if ret:
-    #         cv2.imshow('test', frame)
-    #
-    #     if cv2.waitKey(1) & 0xff == ord('q'):
-    #         break
-    # cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
     main()
